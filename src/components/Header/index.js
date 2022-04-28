78fb28682725f12fe815fbc093a463b78a66f8ab
Sam Bulatov <mephistorine@gmail.com>
2022-04-28 15:32:53 +0300

    feat: При прокрутке вниз хеадер уменьшается в высоте

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -3,13 +3,26 @@ import Container from "../Container"
 
 import logoPng from "../../assets/logo.png"
 import cn from "classnames"
+import { useEffect, useState } from "react"
 
 const MENU = ['Menu 1', 'Menu 2', 'Menu 3', 'Menu 4'];
 
 export default function Header() {
+  const [ isTightHeader, setIsTightHeader ] = useState(false)
+  
+  useEffect(() => {
+    const handleWindowScroll = () => setIsTightHeader(window.scrollY > 60)
+    
+    window.addEventListener("scroll", handleWindowScroll)
+
+    return () => {
+      window.removeEventListener("scroll", handleWindowScroll)
+    }
+  }, [])
+  
   return (
     <header className={ classes.root }>
-      <div className={ classes.header }>
+      <div className={ cn(classes.header, { [ classes.small ]: isTightHeader }) }>
         <Container className={classes.headerWrap}>
           <div className={ classes.logo }>
             <img src={ logoPng } alt="Logo" />
