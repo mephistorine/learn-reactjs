d6d5f73c30d1f270d58a207b4992dfb98dc36f2b
Sam Bulatov <mephistorine@gmail.com>
2022-05-09 15:41:36 +0300

    feat: Домашнее задание 6

--- src/App.js
+++ src/App.js
@@ -1,13 +1,29 @@
 import "./App.module.scss"
-import { Route, Routes } from "react-router-dom"
+import { Route, Routes, useLocation, useNavigate } from "react-router-dom"
 import Main from "./pages/Main"
 import Biography from "./pages/Biography"
 import Layout from "./components/Layout/Layout"
 import Contacts from "./pages/Contacts"
 import About from "./pages/About/About"
 import Characters from "./pages/Characters/Characters"
+import NotFoundPage from "./pages/NotFoundPage"
+import { useEffect } from "react"
 
 function App() {
+  const location = useLocation();
+  
+  useEffect(() => {
+    if (location.hash) {
+      const el = document.querySelector(location.hash)
+      el.scrollIntoView({
+        block: 'center',
+        behavior: 'smooth'
+      })
+    } else {
+      window.scrollTo({ top: 0 })
+    }
+  }, [ location.pathname, location.hash ])
+  
   return (
     <Routes>
       <Route path="/" element={<Layout />}>
@@ -16,6 +32,7 @@ function App() {
         <Route path="about" element={<About />} />
         <Route path="characters" element={ <Characters /> } />
         <Route path="characters/:id" element={ <Biography /> } />
+        <Route path="*" element={ <NotFoundPage /> } />
       </Route>
     </Routes>
   )
--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,6 +1,7 @@
 import Header from "../Header"
 import Footer from "../Footer"
 import { Outlet } from "react-router-dom"
+import { useEffect } from "react"
 
 export default function Layout() {
   return (
